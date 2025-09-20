43d11de3904c18f481b04ab5b025a7892983ead8
Pink00Monster <vanillaWangyi@163.com>
2025-09-20 10:04:25 +0200

    Add project files.

---
+++ JwtAuthDotNet9/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using JwtAuthDotNet9.Entities;
+using JwtAuthDotNet9.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDotNet9.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register (UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+            return Ok(user);
+        }
+    }
+}
